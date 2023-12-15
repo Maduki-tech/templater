88f7da3cec7ad05b180e825d62db612b2331da04
Maduki-tech <d.schlueter1011@gmail.com>
2023-12-15 17:53:45 +0100

    added comments and clear window after use

--- src/handleFiles/handleFiles.go
+++ src/handleFiles/handleFiles.go
@@ -2,12 +2,31 @@ package handleFiles
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
 )
 
 func HandleFiles(selectedItem string) {
 	switch selectedItem {
 	case "C":
-	CreateCFile("makefile")
+		CreateCFile("makefile")
+		cmd := exec.Command("clear") //Linux example, its tested
+        cmd.Stdout = os.Stdout
+        cmd.Run()
+
+		println(`
+Succesfully created C file with makefile.
+-------------------------------------------
+To compile the project run: make
+To run the project run: ./main
+
+To clean the project run: make clean
+To create a compile_commands.json file run: make compile_commands
+
+-------------------------------------------
+If you like the project please star it on github:
+www.github.com/maduki-tech/templater
+`)
 	case "C++":
 		fmt.Println("C++")
 	}
